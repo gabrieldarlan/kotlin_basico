e035b8aa60447ddd40a60495a924df50c0dd00f9
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-04-28 09:36:17 -0300

    cap 2 completo

--- src/main.kt
+++ src/main.kt
@@ -1,12 +1,39 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
-    saldo = 100 + 2.0
-    saldo += 200
 
-    println("titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+        i++
+    }
+/*    for (i in 5 downTo 1) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
+
+        println("titular $titular")
+        println("número da conta $numeroConta")
+        println("saldo da conta $saldo")
+        println()
+
+//    testaCondicoes(saldo)
+
+    }*/
 }
+
+private fun testaCondicoes(saldo: Double) {
+    //o when substitui uma cadeia de if's e else if's
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        saldo < 0.0 -> println("conta é negativa")
+    }
+}
+
