b73a83175350225b39d8f0703e46775481010461
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-04-28 11:31:18 -0300

    cap 3 completo

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,62 @@
 fun main() {
     println("Bem vindo ao Bytebank")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
 
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    contaAlex.saldo += 50.0
+    println("depositando na conta do alex")
+    deposita(contaAlex, 50.0)
+    println(contaAlex.saldo)
+    println("depositando na conta da fran")
+    deposita(contaFran, 70.0)
+    println(contaFran.saldo)
+}
+
+fun deposita(conta: Conta, valor: Double) {
+    conta.saldo += valor
+}
+
+private fun testaCopiasEReferencias() {
+    val numerox = 10
+    var numeroY = numerox
+    numeroY++
+
+    println("numerox $numerox")
+    println("numeroY $numeroY")
+
+    val contaJoao = Conta()
+    contaJoao.titular = "joao"
+    var contaMaria = Conta()
+    contaMaria.titular = "maria"
+
+    println("titular conta joao: ${contaJoao.titular} ")
+    println("titular conta maria: ${contaMaria.titular} ")
+}
+
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+
+}
+
+private fun testaLacos() {
     var i = 0
     while (i < 5) {
         val titular: String = "Alex $i"
@@ -22,8 +78,6 @@ fun main() {
         println("número da conta $numeroConta")
         println("saldo da conta $saldo")
         println()
-
-//    testaCondicoes(saldo)
 
     }*/
 }
