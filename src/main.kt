8815ec896bf55fb7d7c420f6ec3fc724b86c5558
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-04-29 14:42:17 -0300

    curso completo

--- src/main.kt
+++ src/main.kt
@@ -1,66 +1,30 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    val contaAlex = Conta()
-    contaAlex.titular = "Alex"
-    contaAlex.numero = 1000
-    contaAlex.setSaldo(200.0)
-
-    val contaFran = Conta()
-    contaFran.titular = "Fran"
-    contaFran.numero = 1001
-    contaFran.setSaldo(300.0)
-
-    println(contaFran.titular)
-    println(contaFran.numero)
-    println(contaFran.getSaldo())
-
-    println(contaAlex.titular)
-    println(contaAlex.numero)
-    println(contaAlex.getSaldo())
-
-//    contaAlex.saldo += 50.0
-//    println("depositando na conta do alex")
-//    contaAlex.deposita(50.0)
-//    println(contaAlex.saldo)
-//    println("depositando na conta da fran")
-//    contaFran.deposita(70.0)
+    val contaAlex = Conta(titular = "Alex",numero =  1000)
+    contaAlex.deposita(200.0)
+    val contaFran = Conta(numero = 1001, titular = "fran")
+    contaFran.deposita(300.0)
+//    println(contaFran.titular)
+//    println(contaFran.numero)
 //    println(contaFran.saldo)
-//
-//    println("sacando na conta do alex")
-//    contaAlex.saca(250.0)
+//    println(contaAlex.titular)
+//    println(contaAlex.numero)
 //    println(contaAlex.saldo)
-//
-//    println("sacando na conta da fran")
-//    contaAlex.saca(100.0)
-//    println(contaAlex.saldo)
-//
-//    println("saque em excesso alex")
-//    contaAlex.saca(100.0)
-//    println(contaAlex.saldo)
-//
-//    println("saque em excesso fran")
-//    contaAlex.saca(500.0)
-//    println(contaAlex.saldo)
-//
-//    println("transferencia da conta da fran pra o alex")
-//    if (contaFran.transfere(1150.0, contaAlex)) {
-//        println("deu certo")
-//    } else {
-//        println("nao deu")
-//    }
-//    println("saldo alex: ${contaAlex.saldo}")
-//    println("saldo fran: ${contaFran.saldo}")
 
 }
 
-class Conta {
+class Conta(
+    var titular: String,
+    val numero: Int = 0
+) {
 
-    var titular = ""
-    var numero = 0
-    private var saldo = 0.0
+    var saldo = 0.0
+        private set
 
     fun deposita(valor: Double) {
-        this.saldo += valor
+        if (valor > 0) {
+            this.saldo += valor
+        }
     }
 
     fun saca(valor: Double) {
@@ -78,45 +42,35 @@ class Conta {
         return false
     }
 
-    fun getSaldo(): Double {
-        return saldo
+
+    private fun testaCopiasEReferencias() {
+        val numerox = 10
+        var numeroY = numerox
+        numeroY++
+
+        println("numerox $numerox")
+        println("numeroY $numeroY")
+
+        val contaJoao = Conta("Joao", 1002)
+        val contaMaria = Conta("maria", 1003)
+
+        println("titular conta joao: ${contaJoao.titular} ")
+        println("titular conta maria: ${contaMaria.titular} ")
     }
 
-    fun setSaldo(valor: Double) {
-        saldo = valor
-    }
-}
+    private fun testaLacos() {
+        var i = 0
+        while (i < 5) {
+            val titular: String = "Alex $i"
+            val numeroConta: Int = 1000 + i
+            val saldo: Double = i + 10.0
 
-private fun testaCopiasEReferencias() {
-    val numerox = 10
-    var numeroY = numerox
-    numeroY++
-
-    println("numerox $numerox")
-    println("numeroY $numeroY")
-
-    val contaJoao = Conta()
-    contaJoao.titular = "joao"
-    var contaMaria = Conta()
-    contaMaria.titular = "maria"
-
-    println("titular conta joao: ${contaJoao.titular} ")
-    println("titular conta maria: ${contaMaria.titular} ")
-}
-
-private fun testaLacos() {
-    var i = 0
-    while (i < 5) {
-        val titular: String = "Alex $i"
-        val numeroConta: Int = 1000 + i
-        var saldo: Double = i + 10.0
-
-        println("titular $titular")
-        println("número da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
-        i++
-    }
+            println("titular $titular")
+            println("número da conta $numeroConta")
+            println("saldo da conta $saldo")
+            println()
+            i++
+        }
 /*    for (i in 5 downTo 1) {
         val titular: String = "Alex $i"
         val numeroConta: Int = 1000 + i
@@ -128,6 +82,7 @@ private fun testaLacos() {
         println()
 
     }*/
+    }
 }
 
 private fun testaCondicoes(saldo: Double) {
